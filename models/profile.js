c59b961a2807d0099e861a175088cb9e64b41372
Erodri6z <erikrodriguez2417@gmail.com>
2023-10-14 21:12:43 -0500

    stub up a model for profile

--- models/profile.js
+++ models/profile.js
@@ -3,8 +3,28 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
-  name: String,
-  photo: String
+  fullName:{
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  authorazationLevel: {
+    type: String,
+    required: true
+  },
+  zipCode: {
+    type: String,
+    required: true
+  },
+  myStores: [{type: Schema.Types.ObjectId, ref: 'Business'}],
+  productWishlist: [{type: Schema.Types.ObjectId, ref:'Products'}],
+  rating: {
+    type: Number,
+    required: false
+  }
 },{
   timestamps: true,
 })
