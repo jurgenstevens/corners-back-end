5f34cd3d6d7c74ef9bc2481749bcb80fff372be6
Erodri6z <erikrodriguez2417@gmail.com>
2023-10-16 23:34:50 -0500

    stub up create function

--- controllers/businesses.js
+++ controllers/businesses.js
@@ -1,2 +1,40 @@
 import { Business } from "../models/business";
 
+async function create(req, res) {
+  try{
+    req.body.businessOwnerName = req.user.profile.fullName
+    Business.create(req.body)
+    .then(business => {
+      Business.findById(business._id)
+      .populate([
+        {
+          path: "patrons"
+        },{
+          path: "customerProductOptions"
+        },{
+          path: "productsPendingApproval"
+        },{
+          path: "recentlyAddedProducts"
+        },{
+          path: "rejectedProducts"
+        },{
+          path: "productsOnSale"
+        }
+      ])
+      .then(popBusiness => {
+        res.json(popBusiness)
+      })
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
+
+
+
+
+
+
+export { create }
