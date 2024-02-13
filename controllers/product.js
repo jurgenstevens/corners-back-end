d9106c323be3797a275a022d08274fe4db02fa09
Erodri6z <erikrodriguez2417@gmail.com>
2024-02-12 18:40:14 -0600

    stub up index for products

--- controllers/product.js
+++ controllers/product.js
@@ -1,1 +1,20 @@
- 
+
+import { Product } from "../models/product"
+
+async function index(req, res) {
+  try{
+    Product.find({})
+    .then(products =>
+      res.json(products)
+    )
+
+  } catch (err){
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
+
+export {
+  index
+}
